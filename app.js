64f3d08f60d742d19ac164f0251963f00758a672
Computer <computer@Computers-MacBook-Pro.local>
2018-12-13 16:02:58 -0700

    A Q + A

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("working");
+
+// Variable & datatypes
+
+//1
+//using the = sign
+
+//2
+// by changing what is on the right side of the = sign
+
+//3
+//take the value of the var and set it to a new var
+
+//4
+// when you declare a variable it registers a variable in the corresponding scope and when you assign a variable it assigns a specified value to the variable.
+
+//5
+// its when you write out the steps of the code in normal words rather than in a coding languages.
+//everytime youre writing new code.
+
+//6
+//until you understand the steps involved to get to the final answer.
